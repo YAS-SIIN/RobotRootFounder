80b713a6c00aadf1780528e478b0a0e9660d66d1
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-12-22 02:40:02 +0330

    Add project files.

---
+++ NfiCodeAssignment/Services/RobotScheduleValidationService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NfiCodeAssignment.Services
+{
+    public class RobotScheduleValidationService
+    {
+        /// <summary>
+        /// Validates whether there are no collisions between any of the robots in the schedule.
+        /// </summary>
+        /// <param name="schedule">The schedule, as read from a file. See the "Readme.pdf" file in the root folder of
+        /// this project for more information about the structure/contents of this string. </param>
+        /// <returns>Whether the schedule is valid. The schedule is valid if there are no collisions between any of the
+        /// robots.</returns>
+        public bool ValidateSchedule(string schedule)
+        {
+            var lines = ParseScheduleInputLines(schedule);
+            
+            // TODO: implement this function.
+            // This function should return `true` if the schedule is valid (no collisions).
+            // If there is a collision in any of the time steps, this function should return `false`.
+            // Of course, you are allowed to create additional functions and classes. Don't put all code into one huge function!
+
+            // Check the "Readme.pdf" file in the root folder of this project for instructions!
+            // Once you have finished implementing this function and all test cases succeed, you are done.
+            
+            return false;
+        }
+
+        /// <summary>
+        /// Extracts the lines from the schedule string.
+        /// Removes all comments and empty lines.
+        /// </summary>
+        private List<string> ParseScheduleInputLines(string schedule)
+        {
+            return schedule
+                .Replace("\r\n", "\n")
+                .Split('\n')
+                .Where(x => !string.IsNullOrWhiteSpace(x) && !x.StartsWith("#"))
+                .ToList();
+        }
+    }
+}
